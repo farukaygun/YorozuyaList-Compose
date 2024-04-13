b77fcd37339903c328f50ba81847ae8fdd4e5e7b
Faruk Aygun <farukaygun1@outlook.com>
2024-04-13 21:43:39 +0300

    Bump version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,31 +53,31 @@ android {
 }
 
 dependencies {
-	implementation("com.google.android.engage:engage-core:1.3.0")
-	val hiltVersion = "2.48.1"
+	implementation("com.google.android.engage:engage-core:1.4.0")
+	val hiltVersion = "2.51.1"
 	val ktorVersion = "2.3.5"
 	val coilVersion = "2.4.0"
 
 	implementation("androidx.core:core-ktx:1.12.0")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-	implementation("androidx.activity:activity-compose:1.8.0")
-	implementation(platform("androidx.compose:compose-bom:2023.10.00"))
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+	implementation("androidx.activity:activity-compose:1.8.2")
+	implementation(platform("androidx.compose:compose-bom:2024.04.00"))
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
-	implementation("androidx.compose.material3:material3:1.1.2")
+	implementation("androidx.compose.material3:material3:1.2.1")
 
 	// navigation
-	implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-	implementation("androidx.navigation:navigation-compose:2.7.4")
+	implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+	implementation("androidx.navigation:navigation-compose:2.7.7")
 
 	//Dagger - Hilt
 	implementation("com.google.dagger:hilt-android:$hiltVersion")
 	ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
-	ksp("androidx.hilt:hilt-compiler:1.0.0")
-	implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-	implementation("androidx.hilt:hilt-work:1.0.0")
-	implementation("androidx.work:work-runtime-ktx:2.8.1")
+	ksp("androidx.hilt:hilt-compiler:1.2.0")
+	implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+	implementation("androidx.hilt:hilt-work:1.2.0")
+	implementation("androidx.work:work-runtime-ktx:2.9.0")
 
 	// Ktor
 	implementation("io.ktor:ktor-client-core:$ktorVersion")
@@ -90,14 +90,14 @@ dependencies {
 	// Coil
 	implementation("io.coil-kt:coil-compose:$coilVersion")
 	// Custom Tabs
-	implementation("androidx.browser:browser:1.6.0")
+	implementation("androidx.browser:browser:1.8.0")
 	implementation("androidx.preference:preference-ktx:1.2.1")
 
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
+	androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 	debugImplementation("androidx.compose.ui:ui-tooling")
-	debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.3")
+	debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.5")
 }
