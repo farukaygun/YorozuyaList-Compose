53b0b4a9931a7bf6a1f07fca6aeac9d716a5d62d
Faruk Aygun <farukaygun1@outlook.com>
2024-07-05 14:52:16 +0300

    Github Actions config for dev branch.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,10 @@ android {
 		}
 	}
 
+	tasks.register("printVersionName") {
+		println(android.defaultConfig.versionName)
+	}
+
 	buildTypes {
 		release {
 			isMinifyEnabled = false
@@ -59,7 +63,7 @@ dependencies {
 
 	implementation("com.google.android.engage:engage-core:1.5.2")
 	implementation("androidx.core:core-ktx:1.13.1")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
 	implementation("androidx.activity:activity-compose:1.9.0")
 	implementation(platform("androidx.compose:compose-bom:2024.06.00"))
 	implementation("androidx.compose.ui:ui")
@@ -68,7 +72,7 @@ dependencies {
 	implementation("androidx.compose.material3:material3:1.2.1")
 
 	// navigation
-	implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
+	implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
 	implementation("androidx.navigation:navigation-compose:2.8.0-beta04")
 
 	// Ktor
